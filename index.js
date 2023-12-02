f3b8ae959ba3cc09a866eea787ac065d55aae9a6
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2023-12-01 21:21:22 -0500

    v1

---
+++ index.js
@@ -0,0 +1,9 @@
+function select() {
+  // changes the header
+  document.getElementById("table-view").classList.toggle("selected")
+  document.getElementById("table-view").classList.toggle("not-selected")
+  document.getElementById("gallery-view").classList.toggle("not-selected")
+  document.getElementById("gallery-view").classList.toggle("selected")
+
+  // changes the body
+}
