cd759066890511852766865035517acf1c2c5354
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-04-02 16:46:40 -0400

    updated

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function select() {
   document.getElementById("table-view-body").classList.toggle("not-selected");
   document.getElementById("gallery-view-body").classList.toggle("not-selected");
 }
+
+function selectResume() {
+  document.getElementById("gallery-view-body").classList.remove("selected");
+  document.getElementById("gallery-view-body").classList.add("not-selected");
+
+  document.getElementById("table-view-body").classList.add("selected");
+  document.getElementById("table-view-body").classList.remove("not-selected");
+
+  document.getElementById("table-view").classList.add("selected-nav")
+  document.getElementById("gallery-view").classList.remove("selected-nav")
+}
+
+function selectMedia() {
+  document.getElementById("gallery-view-body").classList.remove("not-selected");
+  document.getElementById("gallery-view-body").classList.add("selected");
+
+  document.getElementById("table-view-body").classList.add("not-selected");
+  document.getElementById("table-view-body").classList.remove("selected");
+
+  document.getElementById("table-view").classList.remove("selected-nav")
+  document.getElementById("gallery-view").classList.add("selected-nav")
+}
